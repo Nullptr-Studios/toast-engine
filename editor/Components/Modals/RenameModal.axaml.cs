cb3cf2caf568d1479b62262d00de1e18e07cbee3
Xein <xgonip@gmail.com>
2026-07-31 21:52:51 +0200

    Popup overhaul

--- editor/Components/Modals/RenameModal.axaml.cs
+++ editor/Components/Modals/RenameModal.axaml.cs
@@ -1,6 +1,5 @@
 using System;
 using Avalonia.Controls;
-using Avalonia.Input;
 using Avalonia.Interactivity;
 
 namespace editor.Components.Modals;
@@ -24,14 +23,5 @@ public partial class RenameModal : Window {
 
 	private void OnCancel(object? sender, RoutedEventArgs e) {
 		Close(null);
-	}
-
-	private void OnKeyDown(object? sender, KeyEventArgs e) {
-		if (e.Key == Key.Enter) {
-			var name = NameBox.Text?.Trim();
-			if (!string.IsNullOrEmpty(name)) Close(name);
-		} else if (e.Key == Key.Escape) {
-			Close(null);
-		}
 	}
 }
